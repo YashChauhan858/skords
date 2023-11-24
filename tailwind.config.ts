4b11582873becdcb87e6b02f4eb0e6c91e19a911
Yash Chauhan <yashchauhan858@gmail.com>
2023-11-24 21:13:07 +0530

    added config colors for tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,21 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        textColor: "#e2e8f0",
+        textColorMuted: "#8e9cb1",
+        badgeBg: "#112b3a",
+        badgeText: "#53d0bf",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
