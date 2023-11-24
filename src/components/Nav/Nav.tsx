98e390c28c1d127f764db91fab5010002d2d3948
Yash Chauhan <yashchauhan858@gmail.com>
2023-11-24 21:32:58 +0530

    Added styling to nav bar

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -2,12 +2,12 @@ import Link from "next/link";
 
 const Nav = () => {
   return (
-    <nav className=" p-6">
+    <nav className="p-6 px-24">
       <ul className="flex gap-10">
-        <li className="select-none cursor-pointer">
+        <li className="select-none cursor-pointer hover:text-badgeText transition-all duration-200">
           <Link href={"/portfolio"}>Portfolio</Link>
         </li>
-        <li className="select-none cursor-pointer">
+        <li className="select-none cursor-pointer hover:text-badgeText transition-all duration-200">
           <Link href={"/contact"}>Contact</Link>
         </li>
       </ul>
