a58448e8ff153377c07897b9b5937fd2c0e31bee
Yash Chauhan <yashchauhan858@gmail.com>
2023-11-25 11:31:39 +0530

    Added logo to nav bar

--- src/components/Global/Nav/Nav.tsx
+++ src/components/Global/Nav/Nav.tsx
@@ -1,9 +1,15 @@
+import Image from "next/image";
 import Link from "next/link";
 
 const Nav = () => {
   return (
     <nav className="p-6 px-24 sticky top-0">
-      <ul className="flex gap-10">
+      <ul className="flex gap-10 items-center">
+        <li className="select-none cursor-pointer">
+          <Link href={"/"}>
+            <Image src={"/logo.png"} alt="logo" height={40} width={40} />
+          </Link>
+        </li>
         <li className="select-none cursor-pointer  transition-all duration-200 px-2 py-1 rounded-full hover:text-badgeText hover:bg-badgeBg">
           <Link href={"/portfolio"}>Portfolio</Link>
         </li>
