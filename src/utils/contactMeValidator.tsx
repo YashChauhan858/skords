1caca93c29df54138cc67efecf85f7bb03e877eb
Yash Chauhan <yashchauhan858@gmail.com>
2023-11-25 09:51:14 +0530

    Added form validator function

---
+++ src/utils/contactMeValidator.tsx
@@ -0,0 +1,30 @@
+interface IContactFormValidator {
+  email: string;
+  contactNumber: string;
+  subject: string;
+}
+
+const contactMeValidator = (
+  formData: IContactFormValidator
+): [boolean, IContactFormValidator] => {
+  const errorObject = {
+    email: "",
+    contactNumber: "",
+    subject: "",
+  };
+  if (formData.email === "") {
+    errorObject.email = "Please add a valid email address";
+  }
+  if (formData.contactNumber.length < 10 || formData.contactNumber === "") {
+    errorObject.contactNumber = "Please add a valid contact address";
+  }
+  if (formData.subject === "") {
+    errorObject.subject = "Please add a subject to discuss";
+  }
+  if (Object.values(errorObject).some((e) => e !== "")) {
+    return [true, errorObject];
+  }
+  return [false, errorObject];
+};
+
+export default contactMeValidator;
