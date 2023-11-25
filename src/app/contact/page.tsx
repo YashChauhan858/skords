689c580b2a71b7936f3af5d38e6414a4577fa547
Yash Chauhan <yashchauhan858@gmail.com>
2023-11-25 09:51:48 +0530

    Added Contact page

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,11 @@
+import ContactMeForm from "@/components/ContactMeForm/ContactMeForm";
+
+const contact = () => {
+  return (
+    <main>
+      <ContactMeForm />
+    </main>
+  );
+};
+
+export default contact;
