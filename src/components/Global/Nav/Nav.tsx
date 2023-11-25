af40d6234a46fb1801efbec46ed4fe0042433172
Yash Chauhan <yashchauhan858@gmail.com>
2023-11-25 08:40:16 +0530

    hover style added

--- src/components/Global/Nav/Nav.tsx
+++ src/components/Global/Nav/Nav.tsx
@@ -4,10 +4,10 @@ const Nav = () => {
   return (
     <nav className="p-6 px-24 sticky top-0">
       <ul className="flex gap-10">
-        <li className="select-none cursor-pointer hover:text-badgeText transition-all duration-200">
+        <li className="select-none cursor-pointer  transition-all duration-200 px-2 py-1 rounded-full hover:text-badgeText hover:bg-badgeBg">
           <Link href={"/portfolio"}>Portfolio</Link>
         </li>
-        <li className="select-none cursor-pointer hover:text-badgeText transition-all duration-200">
+        <li className="select-none cursor-pointer  transition-all duration-200 px-2 py-1 rounded-full hover:text-badgeText hover:bg-badgeBg">
           <Link href={"/contact"}>Contact</Link>
         </li>
       </ul>
